dee00a26ab3871c685fa65e41921462019fac19d
Eugene Lee <yvjnlee@gmail.com>
2025-09-13 02:08:28 -0400

    first memecoin

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
 // TODO: Implement
 contract Token { 
-    constructor() {}
+    constructor() ERC20("GooseCoin", "GOOSE") Ownable(msg.sender) {
+      _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+      _mint(to, amount);
+    }
 }
--- ignition/modules/Token.ts
+++ ignition/modules/Token.ts
@@ -2,6 +2,6 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("Token", (m) => {
   // Replace this with the initial supply you want to mint to the deployer
-  const token = m.contract("Token", ["INITIAL_SUPPLY"]);
+  const token = m.contract("Token", ["67"]);
   return { token };
 });
